3cb775e0d7872d1ed3cd58508566c1e49ad35667
mitanvir05 <tanvir.cse05@gmail.com>
2022-05-15 12:25:20 +0600

    date picker added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Banner from "./Banner";
-import Footer from "./Footer";
+import Footer from "../Shared/Footer";
 import Info from "./Info";
 import MakeAppointment from "./MakeAppointment";
 import Services from "./Services";
